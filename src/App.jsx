5420cb6c51fa869e6c845e55c999d9f5f09025ac
Aileen <aileenpadrontorres511@gmail.com>
2024-03-01 19:09:43 +0000

    part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
+// importar los modulos de firebase
+import appFirebase from '../src/credentials'
+import {getAuth,onAuthStateChanged} from 'firebase/auth'
+const auth = getAuth(appFirebase)
+
+// importar componentes
+import Login from '../src/components/Login'
+import Home from '../src/components/Home'
+
 function App() {
 
+  const [user, setUser] = useState(null)
+
+  onAuthStateChanged(auth, (userFirebase) => {
+    if (userFirebase) {
+      setUser(userFirebase)
+    } else {
+      setUser(null)
+    }
+  })
+
   return (
-    <div></div>
+    <div>
+      {user? <Home email={user.email} /> : <Login />} 
+    </div>
   )
 }
 
